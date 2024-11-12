6d6b6960c6f33c6f70264156596b8514c4d36455
Sehrish <daudsehrish@gmail.com>
2024-11-12 10:49:02 -0800

    submitting in-class assignment for nov 12 with working HTML webpage

---
+++ raining-in-seattle.py
@@ -0,0 +1,14 @@
+import urllib.request
+from flask import Flask
+
+raining_in_seattle = Flask(__name__)
+
+@raining_in_seattle.route("/")
+def is_it_raining_in_seattle():
+    with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
+        is_it_raining_in_seattle = response.read().decode()
+
+    if is_it_raining_in_seattle == "true":
+        return "Yes!"
+    else:
+        return "No!"
